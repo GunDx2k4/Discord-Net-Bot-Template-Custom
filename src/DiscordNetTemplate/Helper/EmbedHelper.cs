5f1962ee8654396021434c006aae84536f80324d
Nguyen Xuan Dung <134990068+GunDx2k4@users.noreply.github.com>
2025-05-21 17:38:52 +0700

    feat(helper): add EmbedHelper and StringHelper
    - Implement StringHelper for Markdown formatting
    - Implement EmbedHelper for building Discord embeds

---
+++ src/DiscordNetTemplate/Helper/EmbedHelper.cs
@@ -0,0 +1,70 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace DiscordNetTemplate.Helper;
+
+public class EmbedHelper(DiscordSocketClient client, StringHelper stringHelper)
+{
+    public EmbedAuthorBuilder GetEmbedAuthorBot()
+    {
+        return new EmbedAuthorBuilder
+        {
+            Name = client.CurrentUser.Username,
+            IconUrl = client.CurrentUser.GetAvatarUrl(),
+            Url = $"https://discord.com/oauth2/authorize?client_id={client.CurrentUser.Id}&permissions=8&scope=bot%20applications.commands"
+        };
+    }
+
+    public EmbedFooterBuilder GetEmbedFooter()
+    {
+        return new EmbedFooterBuilder
+        {
+            Text = $"{client.CurrentUser.Username} [{DateTime.Now.Year}]",
+            IconUrl = client.CurrentUser.GetAvatarUrl(),
+        };
+    }
+    
+    public EmbedBuilder BasicEmbedBuilder(string content, EmbedAuthorBuilder author = null)
+    {
+        return new EmbedBuilder
+        {
+            Color = Color.Blue,
+            Description = stringHelper.BoldText(content),
+            Author = author ?? GetEmbedAuthorBot(),
+            Timestamp = DateTimeOffset.Now
+        };
+    }
+
+    public EmbedBuilder ErrorEmbedBuilder(string content)
+    {
+        return new EmbedBuilder
+        {
+            Color = Color.Red,
+            Description = stringHelper.BoldText(content),
+            Author = GetEmbedAuthorBot(),
+            Timestamp = DateTimeOffset.Now
+        };
+    }
+
+    public EmbedBuilder SuccessEmbedBuilder(string content)
+    {
+        return new EmbedBuilder
+        {
+            Color = Color.Green,
+            Description = stringHelper.BoldText(content),
+            Author = GetEmbedAuthorBot(),
+            Timestamp = DateTimeOffset.Now
+        };
+    }
+
+    public EmbedBuilder WarningEmbedBuilder(string content)
+    {
+        return new EmbedBuilder
+        {
+            Color = Color.Gold,
+            Description = stringHelper.BoldText(content),
+            Author = GetEmbedAuthorBot(),
+            Timestamp = DateTimeOffset.Now
+        };
+    }
+}
---
+++ src/DiscordNetTemplate/Helper/StringHelper.cs
@@ -0,0 +1,69 @@
+﻿namespace DiscordNetTemplate.Helper;
+
+public class StringHelper
+{
+    public string ItalicsText(string text)
+    {
+        return $"*{text}*";
+    }
+
+    public string UnderlineText(string text)
+    {
+        return $"_{text}_";
+    }
+
+    public string BoldText(string text)
+    {
+        return $"**{text}**";
+    }
+
+    public string StrikethroughText(string text)
+    {
+        return $"~~{text}~~";
+    }
+
+    public string SpoilerhText(string text)
+    {
+        return $"|{text}|";
+    }
+
+    public string HeadersText(string text, int level = 1)
+    {
+        switch (level)
+        {
+            case 1:
+                return $"# {text}";
+            case 2:
+                return $"## {text}";
+            case 3:
+                return $"### {text}";
+            default:
+                return $"{text}";
+        }
+    }
+
+    public string MaskedLinks(string text, string link)
+    {
+        return $"[{text}]({link})";
+    }
+
+    public string HighlightText(string text)
+    {
+        return $"`{text}`";
+    }
+
+    public string CodeBlock(string text)
+    {
+        return $"```{text}```";
+    }
+
+    public string BlockQuotes(string text)
+    {
+        return $"> {text}";
+    }
+
+    public string BlockQuotesAll(string text)
+    {
+        return $">>> {text}";
+    }
+}
