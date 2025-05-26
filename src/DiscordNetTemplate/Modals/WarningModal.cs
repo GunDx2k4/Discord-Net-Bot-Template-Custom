1fa16e5fa965c543510fdc9e49e7d1ad1cb1660f
Nguyen Xuan Dung <134990068+GunDx2k4@users.noreply.github.com>
2025-05-26 21:56:13 +0700

    feat(modal): add WarningModal
    - Create modal with input field for reason

---
+++ src/DiscordNetTemplate/Modals/WarningModal.cs
@@ -0,0 +1,12 @@
+﻿using Discord.Interactions;
+
+namespace DiscordNetTemplate.Modals;
+
+public class WarningModal : IModal
+{
+    public string Title => "Warning !!!";
+        
+    [InputLabel("Reason")]
+    [ModalTextInput("reason_msg")]
+    public string Reason { get; set; }
+}
